7458b29b9fb4abe68a14db093fcff7bcadc1ee78
Halytskyi5 <volodyaforschool@gmail.com>
2025-02-21 14:06:36 +0200

    add admin requests

--- app/src/main/java/shop_api/app/controllers/AdminController.java
+++ app/src/main/java/shop_api/app/controllers/AdminController.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import shop_api.app.dtos.UserDto;
+import shop_api.app.entities.ProductEntity;
 import shop_api.app.services.AdminService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -18,6 +22,16 @@ public class AdminController {
     @GetMapping("/welcome")
     public ResponseEntity<String> hello(@RequestParam("admin_id") Long id) {
         return ResponseEntity.ok(this.service.welcome(id));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserDto>> getUsers(@RequestParam("admin_id") Long id) {
+        return ResponseEntity.ok(this.service.getUsers(id));
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductEntity>> getProducts(@RequestParam("admin_id") Long id) {
+        return ResponseEntity.ok(this.service.getProducts(id));
     }
 
 }
--- app/src/main/java/shop_api/app/services/AdminService.java
+++ app/src/main/java/shop_api/app/services/AdminService.java
@@ -3,21 +3,49 @@ package shop_api.app.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import shop_api.app.dtos.UserDto;
+import shop_api.app.entities.ProductEntity;
 import shop_api.app.entities.UserEntity;
 import shop_api.app.exceptions.AppException;
+import shop_api.app.mappers.Mapper;
+import shop_api.app.repositories.ProductRepository;
 import shop_api.app.repositories.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class AdminService {
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
-    public String welcome(Long id) {
+    public String welcome(Long id) throws AppException {
         if (this.authenticateAdmin(id)) {
             return "Hello ADMIN";
         }
-        throw new AppException("User hasn't role ADMIN!", HttpStatus.FORBIDDEN);
+        throw new AppException("Access denied!", HttpStatus.FORBIDDEN);
+    }
+
+    public List<UserDto> getUsers(Long id) {
+        if (this.authenticateAdmin(id)) {
+            List<UserDto> userDtos = new ArrayList<>();
+            for(UserEntity user : this.userRepository.findAll()) {
+                userDtos.add(Mapper.toUserDto(user));
+            }
+            return userDtos;
+        }
+        throw new AppException("Access denied!", HttpStatus.FORBIDDEN);
+    }
+
+    public List<ProductEntity> getProducts(Long id) {
+        if (this.authenticateAdmin(id)) {
+            List<ProductEntity> products = this.productRepository.findAll();
+            return products;
+        }
+        throw new AppException("Access denied!", HttpStatus.FORBIDDEN);
     }
 
     private Boolean authenticateAdmin(Long id) {
